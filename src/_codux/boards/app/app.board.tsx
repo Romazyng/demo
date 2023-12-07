66fed4fbb2b9d5273ea48453b8e48ed36bff4c42
Roman <misteryo.wins@mail.ru>
2023-12-07 21:01:58 +0300

    changes

--- src/_codux/boards/app/app.board.tsx
+++ src/_codux/boards/app/app.board.tsx
@@ -4,4 +4,7 @@ import App from '../../../App';
 export default createBoard({
     name: 'App',
     Board: () => <App />,
+    environmentProps: {
+        canvasBackgroundColor: '#bc1e1e', windowWidth: 1024, windowHeight: 768, windowBackgroundColor: '#ffffff'
+    }
 });
